a7797b7c29fbfa35fbcee42e7326acb9ad0dd29c
Dennis Tobi <denkoksz@gmail.com>
2018-07-25 15:04:41 +0200

    Dennis Track some method

--- src/main/java/activitytracker/Track.java
+++ src/main/java/activitytracker/Track.java
@@ -16,15 +16,42 @@ public class Track {
     }
 
     public Coordinate findMaximumCoordinate(){
+        double maxLatitude = trackpoints.get(0).getCoordinate().getLatitude();
+        double maxLongitude = trackpoints.get(0).getCoordinate().getLongitude();
 
+        for(TrackPoint item: trackpoints){
+            if(item.getCoordinate().getLatitude() > maxLatitude){
+                maxLatitude = item.getCoordinate().getLatitude();
+            }
+            if(item.getCoordinate().getLongitude() > maxLongitude){
+                maxLongitude = item.getCoordinate().getLongitude();
+            }
+        }
+        return new Coordinate(maxLatitude, maxLongitude);
     }
 
     public Coordinate findMinimumCoordinate(){
+        double minLatitude = trackpoints.get(0).getCoordinate().getLatitude();
+        double minLongitude = trackpoints.get(0).getCoordinate().getLongitude();
+
+        for(TrackPoint item: trackpoints){
+            if(item.getCoordinate().getLatitude() < minLatitude){
+                minLatitude = item.getCoordinate().getLatitude();
+            }
+            if(item.getCoordinate().getLongitude() < minLongitude){
+                minLongitude = item.getCoordinate().getLongitude();
+            }
+        }
+        return new Coordinate(minLatitude, minLongitude);
 
     }
 
     public double getDistance(){
-
+        double sum = 0.0;
+        for(int i = 0; i < trackpoints.size()-1; i++){
+            sum += trackpoints.get(i).getDistanceFrom(trackpoints.get(i+1));
+        }
+        return sum;
     }
 
     public double getFullDecrease(){
@@ -32,7 +59,11 @@ public class Track {
     }
 
     public double getFullElevation(){
-
+        double sum = 0.0;
+        for(TrackPoint item: trackpoints){
+            sum += item.getElevation();
+        }
+        return sum;
     }
 
     public double getRectangleArea(){
